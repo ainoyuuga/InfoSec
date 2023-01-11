c58816093eb1136e8c8e6089ad9c8acd1c0764a7
ainoyuuga <ayponyta@gmail.com>
2023-01-11 14:06:56 +0800

    Major changes +home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { UserManagementComponent } from './user-management/user-management.component';
+import { PostManagementComponent } from './post-management/post-management.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    AdminDashboardComponent,
+    UserManagementComponent,
+    PostManagementComponent
   ],
   imports: [
     BrowserModule,
